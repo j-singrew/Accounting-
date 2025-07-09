4b7b24ad0b5c1e4028e7590a680e0ca96e98fdc6
topshager <rem28777@gmail.com>
2025-07-09 23:27:09 +0200

    fix react issue

--- pgms_frontend/src/App.js
+++ pgms_frontend/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from  './pages/home/home';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
 
       </Routes>
     </Router>
