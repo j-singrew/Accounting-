d533486a37c3919b974481d7b6735213c00ee5f4
topshager <rem28777@gmail.com>
2025-07-10 00:01:15 +0200

    meta data  update

--- pgms_frontend/src/pages/home/home.js
+++ pgms_frontend/src/pages/home/home.js
@@ -1,9 +1,21 @@
 import "./home.css"
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
 function Home(){
     return(
+        <>
+        <Helmet>
+        <title>PGMS Accounting Services | Tax & Payroll in George</title>
+        <meta
+          name="description"
+          content="Expert accounting, tax planning, and management support tailored for George, Western Cape businesses."
+        />
+      </Helmet>
         <div className="Header">
             <h1>Header</h1>
         </div>
+        </>
     )
 }
 export default Home;
